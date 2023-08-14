9f2ec43e9b3e52ccdcd4999133452207c7814aba
Aleksandra Artyszko <aleksandra.artyszko@stxnext.pl>
2023-08-14 13:46:11 +0200

    ADD: added mochawsome configuration

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,8 +1,14 @@
+/* eslint-disable global-require, @typescript-eslint/no-var-requires */
+
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  reporter: "cypress-mochawesome-reporter",
   e2e: {
     specPattern: "cypress/e2e/**/*.ts",
     baseUrl: "https://www.google.com",
+    setupNodeEvents(on, config) {
+      require("cypress-mochawesome-reporter/plugin")(on, config);
+    },
   },
 });
