717ba53235ef31611af7b1b85d31a79822448de6
Aleksandra Artyszko <aleksandra.artyszko@stxnext.pl>
2023-08-29 01:56:40 +0200

    ADD: check lint-staged

--- cypress/e2e/1-getting-started/todo.cy.ts
+++ cypress/e2e/1-getting-started/todo.cy.ts
@@ -18,6 +18,7 @@ describe("example to-do app", () => {
     // Since we want to visit the same URL at the start of all our tests,
     // we include it in our beforeEach function so that it runs before each test
     cy.visit("https://example.cypress.io/todo");
+    console.log("test");
   });
 
   it("displays two todo items by default", () => {
