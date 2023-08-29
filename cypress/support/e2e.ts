75cb71b1acddbf1fd438ae4ab384d0d4be91fd7c
Bartosz Brozyna <bartosz.brozyna@stxnext.pl>
2023-08-29 21:42:21 +0200

    EDIT: added chai plugin and custom assertion

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -1,5 +1,18 @@
 import "@cypress/fiddle";
 import "cypress-mochawesome-reporter/register";
 import "cypress-map";
+import chaiIsSorted from "chai-sorted";
 
 import "./commands";
+
+chai.use(chaiIsSorted);
+chai.use((_chai) => {
+  function textTrimmed(expectedValue: string) {
+    const value = this._obj.text().trim();
+    this.assert(
+      value === expectedValue,
+      `expected to find text "${expectedValue}", found "${value}"`,
+    );
+  }
+  _chai.Assertion.addMethod("textTrimmed", textTrimmed);
+});
