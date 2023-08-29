57dee367464a22575b837379b3bf50f9a32c44f3
Aleksandra Artyszko <aleksandra.artyszko@stxnext.pl>
2023-08-29 23:08:54 +0200

    EDIT: fix selector methods format

--- cypress/support/selectors.ts
+++ cypress/support/selectors.ts
@@ -1,6 +1,14 @@
-export const getButtonByText = (value: string) =>
-  cy.get(`button:contains('${value}')`);
-export const getLabelByText = (value: string) =>
-  cy.get(`label:contains('${value}')`);
-export const getDataQa = (value: string) => cy.get(`[data-qa="${value}"]`);
-export const getById = (value: string) => cy.get(`#${value}`);
+export const getButtonByText = (value: string) => {
+  return cy.get(`button:contains('${value}')`);
+};
+export const getLabelByText = (value: string) => {
+  return cy.get(`label:contains('${value}')`);
+};
+
+export const getDataQa = (value: string) => {
+  return cy.get(`[data-qa="${value}"]`);
+};
+
+export const getById = (value: string) => {
+  return cy.get(`#${value}`);
+};
