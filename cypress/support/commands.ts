590d97cd45d258c0d88c555cecc65a9fc24da091
Bartosz Brozyna <bartosz.brozyna@stxnext.pl>
2023-08-30 17:49:42 +0200

    EDIT: removed unused command

--- cypress/support/commands.ts
+++
@@ -1,9 +0,0 @@
-Cypress.Commands.add(
-  "getPropertyArray",
-  { prevSubject: "element" },
-  (subject, property) => {
-    return cy.wrap(subject).then(($els: JQuery) => {
-      cy.wrap(Cypress._.map($els, property));
-    });
-  },
-);
